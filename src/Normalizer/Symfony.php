dc081ff281209acac45ae9ab13c0380b31e98981
Sebastian Twarog <sebastian.twarog@efficioconsulting.com>
2021-12-21 10:54:38 +0100

    Fixme: temporary removed object normalizers from Symfony package

--- src/Normalizer/Symfony.php
+++ src/Normalizer/Symfony.php
@@ -7,9 +7,7 @@ namespace Efficio\Logger\Normalizer;
 use Efficio\Logger\Normalizer\Custom\ExceptionNormalizer;
 use Symfony\Component\Serializer\Exception\ExceptionInterface;
 use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
-use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
 use Symfony\Component\Serializer\Normalizer\JsonSerializableNormalizer;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
 /** @internal */
@@ -23,8 +21,8 @@ final class Symfony implements Normalizer
             new JsonSerializableNormalizer(),
             new DateTimeNormalizer(),
             new ExceptionNormalizer(),
-            new GetSetMethodNormalizer(),
-            new ObjectNormalizer(),
+            # new GetSetMethodNormalizer(), # Investigate why failing on some classes (unknown __STATIC caller)
+            # new ObjectNormalizer(),
         ];
         $this->serializer = new Serializer($normalizers, []);
     }
